c817a857102efd6076976015db48a8347be16ab0
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-05 12:00:26 +0200

    Complete Ej1

---
+++ Pair.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjsTema4
+{
+    public class Pair <T1, T2>
+    {
+        private T1 first;
+        private T2 second;
+        public Pair(T1 first, T2 second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+        public (T1, T2) GetValues()
+        {
+            return (first, second);
+        }
+    }
+}
